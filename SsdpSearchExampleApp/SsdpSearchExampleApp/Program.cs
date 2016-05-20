e4af458f3e06024c3a3330fb93b6f1c04db528c8
nikko <nikko@DESKTOP-7DO05IB>
2016-05-20 12:53:01 +0200

    example created

--- SsdpSearchExampleApp/SsdpSearchExampleApp/Program.cs
+++ SsdpSearchExampleApp/SsdpSearchExampleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rssdp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,41 @@ namespace SsdpSearchExampleApp
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            try
+            {
+                var t = p.SearchForDevices();
+                t.Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task SearchForDevices()
+        {
+            using (var deviceLocator = new SsdpDeviceLocator())
+            {
+
+                //without argument search for all upnp enabled devices
+                //possible arguments uuid [string] and or / search wait Time  
+                var foundDevices = await deviceLocator.SearchAsync();
+
+                foreach (var foundDevice in foundDevices)
+                {
+                    // Device data returned only contains basic device details and location
+                    Console.WriteLine("Found " + foundDevice.Usn +
+                        " at " + foundDevice.DescriptionLocation.ToString());
+
+                    // Can retrieve the full device description easily though.
+                    var fullDevice = await foundDevice.GetDeviceInfo();
+
+                    Console.WriteLine(fullDevice.Manufacturer);
+                    Console.WriteLine(fullDevice.ModelDescription);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
